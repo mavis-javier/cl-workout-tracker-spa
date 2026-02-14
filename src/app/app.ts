9c562085dac8bbee04aa4ed10c7446560901c214
Mavis Javier <mavis.a.javier@gmail.com>
2026-02-14 10:41:08 -0600

    implement resource getMapping in JSON

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+
+interface Greeting {
+  id?: number;
+  content?: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -8,5 +14,12 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('cl-workout-tracker-spa');
+  protected readonly title = 'Demo';
+  greeting: Greeting = {};
+
+  private http = inject(HttpClient); // inject is used to get an HttpClient service and use to GET /resource
+
+  constructor() {
+    this.http.get<Greeting>('resource').subscribe(data => this.greeting = data);
+  }
 }
